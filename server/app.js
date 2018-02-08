ec1e8bd78fa1f51da031e06c0fe83da7518e66c4
qwertwerty21 <sunnyw1212@gmail.com>
2018-02-08 11:33:11 -0800

    update package.json

--- server/app.js
+++ server/app.js
@@ -1,7 +1,3 @@
-// require new relic for monitoring and pinging to prevent downtime must be first
-// line
-require('newrelic');
-
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv').config();
@@ -39,12 +35,12 @@ app.set('view engine', 'jade');
 app.use(flash());
 
 //connect to mongodb
-mongoose.connect(mongoURL, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Connected to mongodb!');
-    }
+mongoose.connect(mongoURL, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to mongodb!');
+  }
 });
 
 //config passport
@@ -57,21 +53,27 @@ app.use(cors());
 //logger middleware
 app.use(logger('dev'));
 //bodyparser middlware
-app.use(bodyParser.json({
+app.use(
+  bodyParser.json({
     limit: '200mb',
     type: 'application/json'
-}));
-app.use(bodyParser.urlencoded({
+  })
+);
+app.use(
+  bodyParser.urlencoded({
     limit: '200mb',
     extended: true,
     parameterLimit: 50000
-}));
+  })
+);
 //express-session middleware
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true
-}));
+  })
+);
 
 //cookieparser middleware
 app.use(cookieParser());
@@ -84,29 +86,39 @@ app.use(passport.session());
 let cacheTime = 86400000 * 7; //7 days
 
 //set static folder  serve static assets normally from this folder
-app.use(express.static(path.join(__dirname, 'public'), {
+app.use(
+  express.static(path.join(__dirname, 'public'), {
     maxAge: cacheTime
-})); //was 'public'
+  })
+); //was 'public'
 
 //global vars
-app.use(function (req, res, next) {
-    res.locals.user = req.user || null;
-    next();
+app.use(function(req, res, next) {
+  res.locals.user = req.user || null;
+  next();
 });
-app.use(require('node-sass-middleware')({
+app.use(
+  require('node-sass-middleware')({
     src: path.join(__dirname, 'public'),
     dest: path.join(__dirname, 'public'),
     indentedSyntax: true,
     sourceMap: true
-}));
+  })
+);
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    next();
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  next();
 });
 
 //routes
@@ -114,37 +126,35 @@ app.use('/api', routes);
 // send file so react build file route appears on all url input handle every
 // other route with index.html, which will contain a script tag to your
 // application's JavaScript file(s).
-app.get('/*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 // app.use('/forms', forms); app.use('/recipepost', recipepost); app.use('/user',
 // user); catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers development error handler will print stacktrace
 if (app.get('env') === 'development') {
-    app
-        .use(function (err, req, res, next) {
-            res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
 
 // production error handler no stacktraces leaked to user
-app
-    .use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
-    });
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 module.exports = app;
